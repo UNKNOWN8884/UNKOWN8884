206ee825158f13e101dbd6a36e9bcd57ed77e682
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-01-14 19:40:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/UNKNOWN8884/UNKOWN8884.git /UNKOWN8884
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /UNKOWN8884
 fi
-cd /EvaMaria
+cd /UNKOWN8884
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
